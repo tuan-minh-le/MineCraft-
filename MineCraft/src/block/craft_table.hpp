d12327914b2288516f07969e1182484b1cb8f426
tuanminh <tuan-minh.le@cpe.fr>
2025-06-11 15:11:22 +0200

    crafting table done-ish

--- MineCraft/src/block/craft_table.hpp
+++ MineCraft/src/block/craft_table.hpp
@@ -1,16 +1,21 @@
 #include "block/interractive.hpp"
 
-class Craft_table : public Interractive{
-    protected:
-
-    public:
-
+class Craft_table : public Block {
+private:
     bool opened;
+    static cgp::mesh_drawable craftingTableInstanced;
+    static cgp::image_structure craftingTableTexture;
+    static bool drawable_initialized;
 
-    void action (int* entier);
+public:
     Craft_table();
-
+    ~Craft_table() = default;
+    
+    void initialize() override;
+    void setup_texture() override;
+    void draw_block_at(const cgp::environment_generic_structure& environment) override;
+    
+    void action(int* entier);
     bool get_opened() const;
     bool& set_opened();
-    
 };
