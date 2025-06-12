a10d261af7235cf93a3a983fc769ed0ec4e3b67b
Alexandre Buisson <alexbuis67@outlook.fr>
2025-06-12 09:18:05 +0200

    test craft_table

--- MineCraft/src/block/craft_table.hpp
+++ MineCraft/src/block/craft_table.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include "block/interractive.hpp"
+#include "inventory/inventory.hpp"
 
 class Craft_table : public Interractive {
 private:
     bool opened;
+    Inventory craft_inventory;
 
 public:
     Craft_table();
@@ -12,7 +14,7 @@ public:
     void setup_texture() override;
     void draw_block_at(const cgp::environment_generic_structure& environment) override;
     
-    void action(int* entier);
+    void action();
     bool get_opened() const;
     bool& set_opened();
 };
